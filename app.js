654d14517787e0fadb39454387210661e55cc629
Aditya-jain-0 <adityajain6870@gmail.com>
2023-05-28 15:12:44 +0530

    Added Comments for code readability

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
-//returns promise hold opt
-mongoose.connect('mongodb://127.0.0.1:27017/myapp')
+mongoose.connect('mongodb://127.0.0.1:27017/myapp')  //default mongodb port
 .then(()=>{console.log("Connection Successfull")})
 .catch((err)=>{console.log(err)});
-//{useNewUrlParser:true,useUnifiedTopology : true}
 
 const playlistSchema = new mongoose.Schema({
     name:{
@@ -18,10 +16,10 @@ const playlistSchema = new mongoose.Schema({
         default:Date.now
     }
 })
-//model
+//create model
 const Playlist = new mongoose.model("Playlist",playlistSchema)
 
-//documents
+//create documents
 
 const createDocument = async()=>{
     try{
@@ -69,11 +67,11 @@ const getDocument = async()=>{
     try{
     // const result = await Playlist.find();
     // const result = await Playlist.find({type:"Back End"});
-    // const result = await Playlist.find({type:"Back End"}).select({name : 1}).limit(1); //0->except name
-    // const result = await Playlist.find({projects : {$gte : 2}})
-    // const result = await Playlist.find({type : {$in : ["Back End","API"]}});
-    // const result = await Playlist.find({type : {$nin : ["Back End","API"]}});
-    const result = await Playlist.find({$or : [{type : "Back End"} , {projects : 0}] })
+    // const result = await Playlist.find({type:"Back End"}).select({name : 1}).limit(1); // name:1(display name if 0 display all except name)
+    // const result = await Playlist.find({projects : {$gte : 2}})    //gte :- greater then equals to
+    // const result = await Playlist.find({type : {$in : ["Back End","API"]}});in :- included
+    // const result = await Playlist.find({type : {$nin : ["Back End","API"]}});nin :- not included
+    const result = await Playlist.find({$or : [{type : "Back End"} , {projects : 0}] }) //or operation either one of the condition satisfies
     console.log(result)
     }catch(err){
         console.log(err)
@@ -82,8 +80,8 @@ const getDocument = async()=>{
 // getDocument();
 const updateDocument = async(_id)=>{
     try{
-        // const result = await Playlist.updateOne({_id},{
-        const result = await Playlist.findByIdAndUpdate({_id},{
+        // const result = await Playlist.updateOne({_id},{   
+        const result = await Playlist.findByIdAndUpdate({_id},{ //display which document is updated
             $set : {type : "Server Side bro"}
         },{
             new : true
